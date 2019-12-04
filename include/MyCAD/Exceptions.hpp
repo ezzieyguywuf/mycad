0379837a67f918aad63e39d4ac32f304c9e19233
ezzieyguywuf <WolfgangESanyer@gmail.com>
2019-12-04 16:13:09 -0500

    Added virtual MyCAD::Exception class.

---
+++ include/MyCAD/Exceptions.hpp
@@ -0,0 +1,44 @@
+/* 
+ * Copyright (C) 2019  Wolfgang E. Sanyer <ezzieyguywuf@gmail.com>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MYCAD_EXCEPTIONS_HEADER
+#define MYCAD_EXCEPTIONS_HEADER
+
+#include <exception>
+#include <string>
+
+namespace MyCAD
+{
+    class Exception : public std::exception
+    {
+        public:
+            Exception(std::string const& message);
+
+            /** @name Rule of three*/
+            ///@{
+            Exception(Exception const& anException);
+            Exception& operator=(Exception const& anException);
+            virtual ~Exception() = 0;
+            //@}
+
+            const char* what() const noexcept override;
+
+        private:
+            std::string myMessage;
+    };
+}
+#endif // MYCAD_EXCEPTIONS_HEADER
