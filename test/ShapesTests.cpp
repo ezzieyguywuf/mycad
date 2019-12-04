1f092988c886eb5cb8b9cc4bdd36479424a5046b
ezzieyguywuf <WolfgangESanyer@gmail.com>
2019-12-04 13:21:51 -0500

    Updated Shapes::Edge test..it passes now!

--- test/ShapesTests.cpp
+++ test/ShapesTests.cpp
@@ -32,20 +32,21 @@ SCENARIO("Basic topological entities wrap geometric constructs")
             }
         }
     }
-    //GIVEN("Two points")
-    //{
-        //MyCAD::Geometry::Point p1(0,0,0);
-        //MyCAD::Geometry::Point p2(10,10,10);
-        //WHEN("an Edge is created with them")
-        //{
-            //MyCAD::Shapes::Edge edge(p1, p2);
-            //THEN("a straight Line should be inferred and created between the two points")
-            //{
-                //MyCAD::Geometry::Line line(p1, p2);
-                //REQUIRE(edge.getLine(), line);
-            //}
-        //}
-    //}
+    GIVEN("a line")
+    {
+        MyCAD::Geometry::Point p1(0,0,0);
+        MyCAD::Geometry::Point p2(10,10,10);
+        MyCAD::Geometry::Line line(p1, p2);
+        WHEN("an Edge is created with it")
+        {
+            MyCAD::Shapes::Edge edge(line);
+            THEN("we should be able to retrive the Line")
+            {
+                MyCAD::Geometry::Line line(p1, p2);
+                REQUIRE(edge.getLine() == line);
+            }
+        }
+    }
 }
 
 SCENARIO("CAD Programs can create Primitive Solids")
