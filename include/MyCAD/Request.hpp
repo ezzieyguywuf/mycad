16e9ec707eac770ef63458e8d8f6c5be0e0c3fbe
ezzieyguywuf <WolfgangESanyer@gmail.com>
2019-12-11 12:44:37 -0500

    Added empty Request definition.

--- include/MyCAD/Request.hpp
+++ include/MyCAD/Request.hpp
@@ -4,3 +4,12 @@
  * a copy of the MPL was not distributed with this file, You can obtain one at
  * https://mozilla.org/MPL/2.0/.
  */
+
+#ifndef MYCAD_REQUEST_HEADER
+#define MYCAD_REQUEST_HEADER
+
+class Request
+{
+
+};
+#endif //MYCAD_REQUEST_HEADER
