9212c477e0a4e1d369e8d7c5130854de5f8df8d1
ezzieyguywuf <WolfgangESanyer@gmail.com>
2019-12-12 09:33:52 -0500

    Updated main loop to accept user input.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,32 @@
 
 #include <MyCAD/Communication.hpp>
 
+#include <iostream>
+
 int main(int argc, char* argv[])
 {
     MyCAD::Communication::Server myServer;
     myServer.processArgs(argc, argv);
+
+    // Main loop
+    while (true)
+    {
+        std::cout << "$> " ;
+        std::string input;
+        std::cin >> input;
+
+        MyCAD::Communication::Request request(input);
+        bool retval = myServer.processRequest(request);
+        if(not retval)
+        {
+            std::cout << "Error: server didn't like your command \"" << input << "\"" << std::endl;
+            continue;
+        }
+        if(myServer.getResponse() == MyCAD::Communication::Server::EXIT)
+        {
+            break;
+        }
+        std::cout << "The server said \"" << myServer.getResponse() << "\"" << std::endl;
+    }
     return 0;
 }
