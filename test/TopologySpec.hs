459e80d0b27216553a48625557824e4084476a81
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-05-27 22:58:03 -0400

    Added failing test for addChordEdgeBetween.

--- test/TopologySpec.hs
+++ test/TopologySpec.hs
@@ -43,6 +43,11 @@ spec = do
             property (prop_doesNotModifyFaces T.addChordEdge)
         it "Creates an Edge with two adjacent Vertex" $ do
             property (prop_hasTwoAdjacentVertices T.addChordEdge)
+    describe "addChordEdgeBetween" $ do
+        it "Does not modify Vertices" $ do
+            property (prop_doesNotModifyVertices'
+                      (T.addFreeVertex . T.addFreeVertex)
+                      addChordEdgeBetween')
     describe "addOpenEdgeLoop" $ do
         it "Appends two to the existing Vertices" $ do
             property (prop_appendsTwoToVertices T.addOpenEdgeLoop)
@@ -72,6 +77,12 @@ spec = do
 --                            Helper Functions
 -- ===========================================================================
 type ModTopo = (T.Topology -> T.Topology)
+
+addChordEdgeBetween' :: T.Topology -> T.Topology
+addChordEdgeBetween' t =
+    let ((v2, _), els) = Map.deleteFindMax $ T.getEdgeLoops t
+        (v1, _) = Map.findMax els
+    in T.addChordEdgeBetween v1 v2
 
 addEdgeToLoop' :: T.Topology -> T.Topology
 addEdgeToLoop' t =
@@ -159,8 +170,8 @@ prop_appendsOneToEdges' p f = prepXaddXAppendsNToY p f 1 T.getEdges
 prop_appendsOneToFaces :: ModTopo -> (T.Topology -> Bool)
 prop_appendsOneToFaces f = addXAppendsNToY f 1 T.getFaces
 
---prop_doesNotModifyVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
---prop_doesNotModifyVertices' p f = prepXaddXDoesNotModifyY p f T.getVertices
+prop_doesNotModifyVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
+prop_doesNotModifyVertices' p f = prepXaddXDoesNotModifyY p f T.getVertices
 
 prop_doesNotModifyEdges :: ModTopo -> (T.Topology -> Bool)
 prop_doesNotModifyEdges f = addXDoesNotModifyY f T.getEdges
