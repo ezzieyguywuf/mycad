b9ee68953160c1429a62b5a00aeaab531bcdc7c4
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-05-28 21:56:35 -0400

    First test passes (again./_production/build/bin/synfigstudio rewriting topology...)

--- src/Topology.hs
+++ src/Topology.hs
@@ -1,319 +1,70 @@
 module Topology
 ( -- | Exported types
-  Vertex(..)
-, Edge(..)
-, Face(..)
-, EdgeLoop(..)
-, Topology(..)
-, VertexID(VertexID)
-, EdgeID(EdgeID)
-, FaceID(FaceID)
-, EdgeLoopID(EdgeLoopID)
+  Topology
+, Element(..)
   -- | Exported functions
-, addFreeVertex
-, addRayEdge
-, addLoopEdge
-, addChordEdge
---, addChordEdgeBetween
-, addOpenEdgeLoop
-, addEdgeToLoop
---, closeEdgeLoop
+, emptyTopology
+, addVertex
 , getVertices
-, getEdges
-, getFaces
-, getEdgeLoops
-, vertexEdges
-, edgeVertices
-, edgeFaces
-, pprTopo
---, printTopology
---, addEdge
-  -- | These come from Type Classes
 )where
 
-import Test.QuickCheck
-import qualified Data.Map as Map
-import Data.Text.Prettyprint.Doc
-
---import qualified Data.Text as Text
+import qualified Data.Graph.Inductive.Graph as Graph
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Maybe
+import Test.QuickCheck (Arbitrary, arbitrary, elements)
 
 -- ===========================================================================
 --                               Data Types
 -- ===========================================================================
--- TODO: I want to redo all of this - I want to move all this "Vertex", "Edge", "Face"
--- stuff to a separate module (maybe..GeoTopology or something?). In here, I would like to
--- remain more general.
---
--- I think we can define a `data Node a` such that each `Node` can have one or more `data
--- Bridge b`. We'll define `getNodeValue :: Node a -> a` and `getBridgeValue :: Bridge a
--- -> a`.
---
--- The idea is that a given Node can have one or more Bridge associated with it. Each Bridge
--- will allow `getOpposite :: Node a -> Bridge b -> Node`.
---
--- Additionally, we'll have `getAdjacent :: Node a -> [b]` which will return the value of
--- all adjacent Bridge that have a type of b
-data Vertex =
-     FreeVertex
-   | RayVertex EdgeID
-   | ChordVertex EdgeID
-   | LoopVertex EdgeID FaceID
-   deriving (Show, Eq)
+type NodeLabel = (Element, Int)
+newtype BridgeLabel = BridgeLabel () deriving (Show)
+newtype Topology =
+    Topology {unTopology :: (Gr NodeLabel BridgeLabel)}
+        deriving (Show)
 
-data Edge   =
-      RayEdge VertexID
-    | LoopEdge VertexID FaceID
-    | ChordEdge VertexID VertexID
-    deriving (Show, Eq)
-
-data EdgeLoop = OpenEdgeLoop VertexID deriving (Show, Eq)
-
-data Face   =
-      LoopFace VertexID EdgeID
-    deriving (Show, Eq)
-
-data Topology =
-      EmptyTopology
-    | Topology Vertices Edges Faces EdgeLoops
-    deriving (Show, Eq)
-
-newtype EdgeID   = EdgeID Int deriving (Show, Eq, Ord)
-newtype EdgeLoopID   = EdgeLoopID Int deriving (Show, Eq, Ord)
-newtype VertexID = VertexID Int deriving (Show, Eq, Ord)
-newtype FaceID   = FaceID Int deriving (Show, Eq, Ord)
-
-type Vertices  = Map.Map VertexID Vertex
-type Edges     = Map.Map EdgeID Edge
-type EdgeLoops = Map.Map EdgeLoopID EdgeLoop
-type Faces     = Map.Map FaceID Face
+data Element = Vertex Int
+             | Edge   Int
+             | Face   Int
+             deriving (Show, Eq)
 
 -- ===========================================================================
 --                               Free Functions
 -- ===========================================================================
-addFreeVertex :: Topology -> Topology
-addFreeVertex t =
-    let (vs, es, fs, els)  = getAll t
-        vid = VertexID $ length vs
-        v   = FreeVertex
-        vs' = Map.insert vid v vs
-    in Topology vs' es fs els
+emptyTopology :: Topology
+emptyTopology = Topology Graph.empty
 
-addRayEdge :: Topology -> Topology
-addRayEdge t =
-    let (vs, es, fs, els)  = getAll t
-        vid = VertexID $ length vs
-        eid = EdgeID $ length es
-        vs' = Map.insert vid (RayVertex eid) vs
-        es' = Map.insert eid (RayEdge vid) es
-    in (Topology vs' es' fs els)
+addVertex :: Topology -> Topology
+addVertex g = addNode Vertex g
 
-addLoopEdge :: Topology -> Topology
-addLoopEdge t =
-    let (vs, es, fs, els)  = getAll t
-        vid = VertexID $ length vs
-        eid = EdgeID $ length es
-        fid = FaceID $ length fs
-        v   = LoopVertex eid fid
-        e   = LoopEdge vid fid
-        f   = LoopFace vid eid
-        vs' = Map.insert vid v vs
-        es' = Map.insert eid e es
-        fs' = Map.insert fid f fs
-    in Topology vs' es' fs' els
+getVertices :: Topology -> [Element]
+getVertices t = getData isVertex t
+-- ===========================================================================
+--                        Private Free Functions
+-- ===========================================================================
+addNode :: (Int -> Element) -> Topology -> Topology
+addNode e t = Topology $ Graph.insNode (n, e') t'
+    where t' = unTopology t
+          n  = length $ Graph.nodes t'
+          e' = (e n, n')
+          n' = countNode isVertex t
 
-addChordEdge :: Topology -> Topology
-addChordEdge t =
-    let (vs, es, fs, els) = getAll t
-        vid  = VertexID $ length vs
-        vid2 = VertexID $ length vs + 1
-        eid  = EdgeID $ length es
-        v1   = RayVertex eid
-        v2   = RayVertex eid
-        e    = ChordEdge vid vid2
-        vs'  = Map.insert vid2 v2 $ Map.insert vid v1 vs
-        es'  = Map.insert eid e es
-    in Topology vs' es' fs els
+countNode :: (NodeLabel -> Bool) -> Topology -> Int
+countNode p t = length . Graph.nodes $ Graph.labfilter p $ unTopology t
 
---addChordEdgeBetween :: VertexID -> Vertex ID -> Topology -> Topology
---addChordEdgeBetween vid1 vd2 t =
-    --let (vs, es, fs, els) = getAll t
-        --vs0 = Map.insert v1 (ChordVertex eid) vs
-        --vs' = Map.insert v2 (ChordVertex eid) vs0
+isVertex :: NodeLabel -> Bool
+isVertex (Vertex _, _) = True
+isVertex _ = False
 
-addOpenEdgeLoop :: Topology -> Topology
-addOpenEdgeLoop t =
-    let (vs, es, fs, els) = getAll (addChordEdge t)
-        (vid, _) = Map.findMax vs
-        elid = EdgeLoopID $ length els
-        els' = Map.insert elid (OpenEdgeLoop vid) els
-    in Topology vs es fs els'
-
-addEdgeToLoop :: EdgeLoopID -> Topology -> Topology
-addEdgeToLoop elid t =
-    let (vs, es, fs, els) = getAll (addFreeVertex t)
-        eid = EdgeID $ length es
-        (OpenEdgeLoop v1) = els Map.! elid
-        (v2, _) = Map.findMax vs
-        vs' = Map.insert v2 (ChordVertex eid) vs
-        es' = Map.insert eid (ChordEdge v1 v2) es
-        els' = Map.insert elid (OpenEdgeLoop v2) els
-    in Topology vs' es' fs els'
-
---closeEdgeLoop :: EdgeLoopID -> Topology -> Topology
---closeEdgeLoop elid t =
-    --let (vs, es, fs, els) = getAll t
-
-getVertices :: Topology -> Vertices
-getVertices EmptyTopology = Map.empty
-getVertices (Topology vs _ _ _) = vs
-
-getEdges :: Topology -> Edges
-getEdges EmptyTopology = Map.empty
-getEdges (Topology _ es _ _) = es
-
-getFaces :: Topology -> Faces
-getFaces EmptyTopology = Map.empty
-getFaces (Topology _ _ fs _) = fs
-
-getEdgeLoops :: Topology -> EdgeLoops
-getEdgeLoops EmptyTopology = Map.empty
-getEdgeLoops (Topology _ _ _ els ) = els
-
-vertexEdges :: VertexID -> Topology -> Maybe Edges
-vertexEdges vid t = do
-    let vs = getVertices t
-        es = getEdges t
-    v <- Map.lookup vid vs
-    case v of
-        (RayVertex eid) -> Just $ filterSubMap [eid] es
-        (ChordVertex eid) -> Just $ filterSubMap [eid] es
-        (LoopVertex eid _) -> Just $ filterSubMap [eid] es
-        FreeVertex -> Nothing
-
-edgeVertices :: EdgeID -> Topology -> Maybe Vertices
-edgeVertices eid t = do
-    let vs = getVertices t
-        es = getEdges t
-    e <- Map.lookup eid es
-    case e of
-        (RayEdge vid) -> Just $ filterSubMap [vid] vs
-        (LoopEdge vid _) -> Just $ filterSubMap [vid] vs
-        (ChordEdge v1 v2) -> Just $ filterSubMap [v1, v2] vs
-
-edgeFaces :: EdgeID -> Topology -> Maybe Faces
-edgeFaces eid t = do
-    let es = getEdges t
-        fs = getFaces t
-    e <- Map.lookup eid es
-    case e of
-        (LoopEdge _ fid) -> Just $ filterSubMap [fid] fs
-        _ -> Nothing
-
---printTopology :: Topology -> Text.Text
---printTopology t =
-    --let (vs, es, fs, els) = getAll t
-    --in printMap vs
-
--- | For pretty printing
-pprTopo :: Topology -> Doc ann
-pprTopo t =
-    case t of
-      EmptyTopology -> pretty "EmptyTopology"
-      (Topology vs es fs els) ->
-          (pprVMap "Vertices" vs) <+> line
-          <+> (pprEMap "Edges" es) <+> line
-          <+> (pprFMap "Faces" fs) <+> line
-          <+> (pprELMap "EdgeLoops" els)
-
-pprVMap :: String -> Map.Map VertexID Vertex -> Doc ann
-pprVMap n m = printMap n m pprVTuple
-
-pprEMap :: String -> Map.Map EdgeID Edge -> Doc ann
-pprEMap n m = printMap n m pprETuple
-
-pprFMap :: String -> Map.Map FaceID Face -> Doc ann
-pprFMap n m = printMap n m pprFTuple
-
-pprELMap :: String -> Map.Map EdgeLoopID EdgeLoop -> Doc ann
-pprELMap n m = printMap n m pprELTuple
-
-pprVTuple :: (VertexID, Vertex) -> Doc ann
-pprVTuple (vid, v) = printTuple vid v pprVID pprVertex
-
-pprETuple :: (EdgeID, Edge) -> Doc ann
-pprETuple (eid, e) = printTuple eid e pprEID pprEdge
-
-pprFTuple :: (FaceID, Face) -> Doc ann
-pprFTuple (fid, f) = printTuple fid f pprFID pprFace
-
-pprELTuple :: (EdgeLoopID, EdgeLoop) -> Doc ann
-pprELTuple (elid, el) = printTuple elid el pprELID pprELoop
-
-pprVertex :: Vertex -> Doc ann
-pprVertex v =
-    case v of
-        FreeVertex -> pretty "FreeV"
-        (RayVertex eid) -> pretty "RayV" <+> pprEID eid
-        (ChordVertex eid) -> pretty "ChordV" <+> pprEID eid
-        (LoopVertex eid fid) -> pretty "ChordV" <+> pprEID eid <+> pprFID fid
-
-pprEdge :: Edge -> Doc ann
-pprEdge e =
-    case e of
-        (RayEdge vid) -> pretty "RayE" <+> pprVID vid
-        (LoopEdge vid fid) -> pretty "LoopE" <+> pprVID vid <+> pprFID fid
-        (ChordEdge v1 v2) -> pretty "ChordE" <+> pprVID v1 <+> pprVID v2
-
-pprFace :: Face -> Doc ann
-pprFace (LoopFace vid eid) = pretty "LoopF" <+> pprVID vid <+> pprEID eid
-
-pprELoop :: EdgeLoop -> Doc ann
-pprELoop (OpenEdgeLoop vid) = pretty "ELoop" <+> pprVID vid
-
-pprVID :: VertexID -> Doc ann
-pprVID (VertexID n) = printID "V" n
-
-pprEID :: EdgeID -> Doc ann
-pprEID (EdgeID n) = printID "E" n
-
-pprFID :: FaceID -> Doc ann
-pprFID (FaceID n) = printID "F" n
-
-pprELID :: EdgeLoopID -> Doc ann
-pprELID (EdgeLoopID n) = printID "EL" n
+getData :: (NodeLabel -> Bool) -> Topology -> [Element]
+getData p t =
+    let t' = unTopology t
+        xs = Graph.nodes $ Graph.labfilter p t'
+    in map fst $ mapMaybe (Graph.lab t') xs
 
 -- ===========================================================================
---                       Private Free Functions
--- ===========================================================================
-filterSubMap :: (Ord k, Eq a) => [k] -> Map.Map k a -> Map.Map k a
-filterSubMap k m = Map.filterWithKey (\x _ -> x `elem` k) m
-
-getAll :: Topology -> (Vertices, Edges, Faces, EdgeLoops)
-getAll t = (getVertices t, getEdges t, getFaces t, getEdgeLoops t)
-
-printID :: String -> Int -> Doc ann
-printID h n = pretty h <> pretty n
-
-printTuple :: a -> b -> (a -> Doc ann) -> (b -> Doc ann) -> Doc ann
-printTuple a b fa fb = fa a <> pretty ":" <+> fb b
-
-printMap :: (Ord k, Eq a) =>
-        String
-        -> Map.Map k a
-        -> ((k, a) -> Doc ann)
-        -> Doc ann
-printMap n m f = (pretty n) <+> (align . vsep) (map f (Map.assocs m))
-
--- ===========================================================================
---                               Type Classes
--- ===========================================================================
--- ===========================================================================
---                             Implementations
+--                            Instances
 -- ===========================================================================
 instance Arbitrary Topology where
     arbitrary = do
-        let t0  = EmptyTopology
-            t1  = addFreeVertex t0
-            t2  = addRayEdge t0
-            t3  = addLoopEdge t0
-        elements [t0, t1, t2, t3]
+        let t0  = emptyTopology
+        elements [t0]
--- test/TopologySpec.hs
+++ test/TopologySpec.hs
@@ -3,72 +3,71 @@ module TopologySpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 import qualified Topology as T
-import qualified Data.Map as Map
 
 spec :: Spec
 spec = do
     describe "addFreeVertex" $ do
         it "Appends one to the existing Vertices" $ do
-            property (prop_appendsOneToVertices T.addFreeVertex)
-        it "Does not modify the Edges" $ do
-            property (prop_doesNotModifyEdges T.addFreeVertex)
-        it "Does not modify the Faces" $ do
-            property (prop_doesNotModifyFaces T.addFreeVertex)
-    describe "addRayEdge" $ do
-        it "Appends one to the existing Vertices" $ do
-            property (prop_appendsOneToVertices T.addRayEdge)
-        it "Appends one to the existing Edges" $ do
-            property (prop_appendsOneToEdges T.addRayEdge)
-        it "Does not modify the Faces" $ do
-            property (prop_doesNotModifyFaces T.addRayEdge)
-        it "Creates an Edge with one adjacent Vertex" $ do
-            property (prop_hasOneAdjacentVertex T.addRayEdge)
-    describe "addLoopEdge" $ do
-        it "Appends one to the existing Vertices" $ do
-            property (prop_appendsOneToVertices T.addLoopEdge)
-        it "Appends one to the existing Edges" $ do
-            property (prop_appendsOneToEdges T.addLoopEdge)
-        it "Appends one to the existing Faces" $ do
-            property (prop_appendsOneToFaces T.addLoopEdge)
-        it "Creates an Edge with one adjacent Vertex" $ do
-            property (prop_hasOneAdjacentVertex T.addLoopEdge)
-        it "Creates an Edge with one adjacent Face" $ do
-            property (prop_hasOneAdjacentFace T.addLoopEdge)
-    describe "addChordEdge" $ do
-        it "Appends two to the existing Vertices" $ do
-            property (prop_appendsTwoToVertices T.addChordEdge)
-        it "Appends one to the existing Edges" $ do
-            property (prop_appendsOneToEdges T.addChordEdge)
-        it "Does not modify Faces" $ do
-            property (prop_doesNotModifyFaces T.addChordEdge)
-        it "Creates an Edge with two adjacent Vertex" $ do
-            property (prop_hasTwoAdjacentVertices T.addChordEdge)
+            property (prop_appendsOneToVertices T.addVertex)
+        --it "Does not modify the Edges" $ do
+            --property (prop_doesNotModifyEdges T.addFreeVertex)
+        --it "Does not modify the Faces" $ do
+            --property (prop_doesNotModifyFaces T.addFreeVertex)
+    --describe "addRayEdge" $ do
+        --it "Appends one to the existing Vertices" $ do
+            --property (prop_appendsOneToVertices T.addRayEdge)
+        --it "Appends one to the existing Edges" $ do
+            --property (prop_appendsOneToEdges T.addRayEdge)
+        --it "Does not modify the Faces" $ do
+            --property (prop_doesNotModifyFaces T.addRayEdge)
+        --it "Creates an Edge with one adjacent Vertex" $ do
+            --property (prop_hasOneAdjacentVertex T.addRayEdge)
+    --describe "addLoopEdge" $ do
+        --it "Appends one to the existing Vertices" $ do
+            --property (prop_appendsOneToVertices T.addLoopEdge)
+        --it "Appends one to the existing Edges" $ do
+            --property (prop_appendsOneToEdges T.addLoopEdge)
+        --it "Appends one to the existing Faces" $ do
+            --property (prop_appendsOneToFaces T.addLoopEdge)
+        --it "Creates an Edge with one adjacent Vertex" $ do
+            --property (prop_hasOneAdjacentVertex T.addLoopEdge)
+        --it "Creates an Edge with one adjacent Face" $ do
+            --property (prop_hasOneAdjacentFace T.addLoopEdge)
+    --describe "addChordEdge" $ do
+        --it "Appends two to the existing Vertices" $ do
+            --property (prop_appendsTwoToVertices T.addChordEdge)
+        --it "Appends one to the existing Edges" $ do
+            --property (prop_appendsOneToEdges T.addChordEdge)
+        --it "Does not modify Faces" $ do
+            --property (prop_doesNotModifyFaces T.addChordEdge)
+        --it "Creates an Edge with two adjacent Vertex" $ do
+            --property (prop_hasTwoAdjacentVertices T.addChordEdge)
     --describe "addChordEdgeBetween" $ do
         --it "Does not modify Vertices" $ do
             --property (prop_doesNotModifyVertices'
                       --(T.addFreeVertex . T.addFreeVertex)
                       --addChordEdgeBetween')
-    describe "addOpenEdgeLoop" $ do
-        it "Appends two to the existing Vertices" $ do
-            property (prop_appendsTwoToVertices T.addOpenEdgeLoop)
-        it "Appends one to the existing Edges" $ do
-            property (prop_appendsOneToEdges T.addOpenEdgeLoop)
-        it "Does not modify Faces" $ do
-            property $ prop_doesNotModifyFaces T.addOpenEdgeLoop
-        it "Appends to to existing EdgeLoops" $ do
-            property (prop_appendsOneToEdgeLoops T.addOpenEdgeLoop)
-    describe "addEdgeToLoop" $ do
-        it "appends one to the existing Vertices" $ do
-            property (prop_appendsOneToVertices' T.addOpenEdgeLoop addEdgeToLoop')
-        it "appends one to the existing Edges" $ do
-            property (prop_appendsOneToEdges' T.addOpenEdgeLoop addEdgeToLoop')
-        it "Does not modify Faces" $ do
-            property (prop_doesNotModifyFaces' T.addOpenEdgeLoop addEdgeToLoop')
-        it "Modifies EdgeLoop to point to newly added Vertex" $ do
-            property (prop_edgeLoopPointsToNewVertex)
-    describe "closeEdgeLoop" $ do
-        it "Does not modify Vertices" $ do
-            id True
+    --describe "addOpenEdgeLoop" $ do
+        --it "Appends two to the existing Vertices" $ do
+            --property (prop_appendsTwoToVertices T.addOpenEdgeLoop)
+        --it "Appends one to the existing Edges" $ do
+            --property (prop_appendsOneToEdges T.addOpenEdgeLoop)
+        --it "Does not modify Faces" $ do
+            --property $ prop_doesNotModifyFaces T.addOpenEdgeLoop
+        --it "Appends to to existing EdgeLoops" $ do
+            --property (prop_appendsOneToEdgeLoops T.addOpenEdgeLoop)
+    --describe "addEdgeToLoop" $ do
+        --it "appends one to the existing Vertices" $ do
+            --property (prop_appendsOneToVertices' T.addOpenEdgeLoop addEdgeToLoop')
+        --it "appends one to the existing Edges" $ do
+            --property (prop_appendsOneToEdges' T.addOpenEdgeLoop addEdgeToLoop')
+        --it "Does not modify Faces" $ do
+            --property (prop_doesNotModifyFaces' T.addOpenEdgeLoop addEdgeToLoop')
+        --it "Modifies EdgeLoop to point to newly added Vertex" $ do
+            --property (prop_edgeLoopPointsToNewVertex)
+    --describe "closeEdgeLoop" $ do
+        --it "Does not modify Vertices" $ do
+            --id True
             --property (prop_doesNotModifyVertices'
                       --(T.addOpenEdgeLoop addEdgeToLoop')
                       --closeEdgeLoop')
@@ -84,124 +83,123 @@ type ModTopo = (T.Topology -> T.Topology)
         --(v1, _) = Map.findMax els
     --in T.addChordEdgeBetween v1 v2
 
-addEdgeToLoop' :: T.Topology -> T.Topology
-addEdgeToLoop' t =
-    let (k, _) = Map.findMax $ T.getEdgeLoops t
-    in T.addEdgeToLoop k t
+--addEdgeToLoop' :: T.Topology -> T.Topology
+--addEdgeToLoop' t =
+    --let (k, _) = Map.findMax $ T.getEdgeLoops t
+    --in T.addEdgeToLoop k t
 
 --closeEdgeLoop' :: T.Topology -> T.Topology
 --closeEdgeLoop' t =
     --let (k, _) = Map.findMax $ T.getEdgeLoops t
     --in T.closeEdgeLoop k t
 
-applyNTimes :: Int -> (a -> a) -> a -> a
-applyNTimes n f val = foldl (\s e -> e s ) val [f | x <- [1..n]]
+--applyNTimes :: Int -> (a -> a) -> a -> a
+--applyNTimes n f val = foldl (\s e -> e s ) val [f | x <- [1..n]]
 
 addXAppendsNToY ::
-    (Ord a, Eq b) =>
-        ModTopo -- what is used to append?
-        -> Int -- how many are appended?
-        -> (T.Topology -> Map.Map a b) -- what are they appended to?
-        -> T.Topology -> Bool -- This is what is left for QuickCheck
+    ModTopo -- what is used to append?
+    -> Int -- how many are appended?
+    -> (T.Topology -> [T.Element]) -- what are they appended to?
+    -> T.Topology -> Bool -- This is what is left for QuickCheck
 addXAppendsNToY = prepXaddXAppendsNToY id
 
 prepXaddXAppendsNToY ::
-    (Ord a, Eq b) =>
-        ModTopo -- what is used to prepare?
-        -> ModTopo -- what is used to append?
-        -> Int -- how many are appended?
-        -> (T.Topology -> Map.Map a b) -- what are they appended to?
-        -> T.Topology -> Bool -- This is what is left for QuickCheck
+    ModTopo -- what is used to prepare?
+    -> ModTopo -- what is used to append?
+    -> Int -- how many are appended?
+    -> (T.Topology -> [T.Element]) -- what are they appended to?
+    -> T.Topology -> Bool -- This is what is left for QuickCheck
 prepXaddXAppendsNToY prepX addX n getY t0 =
     let t     = prepX t0
         t'    = addX t
         xs    = getY t
         xs'   = getY t'
-     in xs == applyNTimes n Map.deleteMax xs'
+     in xs == take (length xs' - n) xs'
 
-addXDoesNotModifyY ::
-    (Ord a, Eq b) =>
-        ModTopo
-        -> (T.Topology -> Map.Map a b)
-        -> T.Topology -> Bool -- this is what is left for QuickCheck
-addXDoesNotModifyY = prepXaddXDoesNotModifyY id
+--addXDoesNotModifyY ::
+    --(Ord a, Eq b) =>
+        --ModTopo
+        -- -> (T.Topology -> Map.Map a b)
+        -- -> T.Topology -> Bool -- this is what is left for QuickCheck
+--addXDoesNotModifyY = prepXaddXDoesNotModifyY id
 
-prepXaddXDoesNotModifyY ::
-    (Ord a, Eq b) =>
-        ModTopo
-        -> ModTopo
-        -> (T.Topology -> Map.Map a b)
-        -> T.Topology -> Bool -- this is what is left for QuickCheck
-prepXaddXDoesNotModifyY prepX addX getY t0 =
-    let t  = prepX t0
-        t' = addX t
-    in (getY t) ==  (getY t')
+--prepXaddXDoesNotModifyY ::
+    --(Ord a, Eq b) =>
+        --ModTopo
+        -- -> ModTopo
+        -- -> (T.Topology -> Map.Map a b)
+        -- -> T.Topology -> Bool -- this is what is left for QuickCheck
+--prepXaddXDoesNotModifyY prepX addX getY t0 =
+    --let t  = prepX t0
+        --t' = addX t
+    --in (getY t) ==  (getY t')
 
-addXCreatesXWithYAdjacentZ ::
-    (Ord a, Eq b, Ord c, Eq d) =>
-        ModTopo
-        -> (T.Topology -> Map.Map a b)
-        -> Int
-        -> (a -> T.Topology -> Maybe (Map.Map c d))
-        -> T.Topology -> Bool -- This part is what is left for QuickCheck
-addXCreatesXWithYAdjacentZ addX getX n getAdjacentZ t =
-    let t'      = addX t
-        (id, _) = Map.findMax $ getX t' -- max should be the X we just added. This is ensured by prop_addXAppendsToY
-        adj     = getAdjacentZ id t'
-    in (length <$> adj) == Just n
+--addXCreatesXWithYAdjacentZ ::
+    --(Ord a, Eq b, Ord c, Eq d) =>
+        --ModTopo
+        -- -> (T.Topology -> Map.Map a b)
+        -- -> Int
+        -- -> (a -> T.Topology -> Maybe (Map.Map c d))
+        -- -> T.Topology -> Bool -- This part is what is left for QuickCheck
+--addXCreatesXWithYAdjacentZ addX getX n getAdjacentZ t =
+    --let t'      = addX t
+        --(id, _) = Map.findMax $ getX t' -- max should be the X we just added. This is ensured by prop_addXAppendsToY
+        --adj     = getAdjacentZ id t'
+    --in (length <$> adj) == Just n
 -- ===========================================================================
 --                            Properties
 -- ===========================================================================
 prop_appendsOneToVertices :: ModTopo -> (T.Topology -> Bool)
 prop_appendsOneToVertices f = addXAppendsNToY f 1 T.getVertices
 
-prop_appendsOneToVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
-prop_appendsOneToVertices' p f = prepXaddXAppendsNToY p f 1 T.getVertices
+--prop_appendsOneToVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
+--prop_appendsOneToVertices' p f = prepXaddXAppendsNToY p f 1 T.getVertices
 
-prop_appendsTwoToVertices :: ModTopo -> (T.Topology -> Bool)
-prop_appendsTwoToVertices f = addXAppendsNToY f 2 T.getVertices
+--prop_appendsTwoToVertices :: ModTopo -> (T.Topology -> Bool)
+--prop_appendsTwoToVertices f = addXAppendsNToY f 2 T.getVertices
 
-prop_appendsOneToEdges :: ModTopo -> (T.Topology -> Bool)
-prop_appendsOneToEdges f = addXAppendsNToY f 1 T.getEdges
+--prop_appendsOneToEdges :: ModTopo -> (T.Topology -> Bool)
+--prop_appendsOneToEdges f = addXAppendsNToY f 1 T.getEdges
 
-prop_appendsOneToEdges' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
-prop_appendsOneToEdges' p f = prepXaddXAppendsNToY p f 1 T.getEdges
+--prop_appendsOneToEdges' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
+--prop_appendsOneToEdges' p f = prepXaddXAppendsNToY p f 1 T.getEdges
 
-prop_appendsOneToFaces :: ModTopo -> (T.Topology -> Bool)
-prop_appendsOneToFaces f = addXAppendsNToY f 1 T.getFaces
+--prop_appendsOneToFaces :: ModTopo -> (T.Topology -> Bool)
+--prop_appendsOneToFaces f = addXAppendsNToY f 1 T.getFaces
 
 --prop_doesNotModifyVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
 --prop_doesNotModifyVertices' p f = prepXaddXDoesNotModifyY p f T.getVertices
 
-prop_doesNotModifyEdges :: ModTopo -> (T.Topology -> Bool)
-prop_doesNotModifyEdges f = addXDoesNotModifyY f T.getEdges
+--prop_doesNotModifyEdges :: ModTopo -> (T.Topology -> Bool)
+--prop_doesNotModifyEdges f = addXDoesNotModifyY f T.getEdges
 
-prop_doesNotModifyFaces :: ModTopo -> (T.Topology -> Bool)
-prop_doesNotModifyFaces f = addXDoesNotModifyY f T.getFaces
+--prop_doesNotModifyFaces :: ModTopo -> (T.Topology -> Bool)
+--prop_doesNotModifyFaces f = addXDoesNotModifyY f T.getFaces
 
-prop_doesNotModifyFaces' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
-prop_doesNotModifyFaces' p f = prepXaddXDoesNotModifyY p f T.getFaces
+--prop_doesNotModifyFaces' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
+--prop_doesNotModifyFaces' p f = prepXaddXDoesNotModifyY p f T.getFaces
 
-prop_appendsOneToEdgeLoops :: ModTopo -> (T.Topology -> Bool)
-prop_appendsOneToEdgeLoops f = addXAppendsNToY f 1 T.getEdgeLoops
+--prop_appendsOneToEdgeLoops :: ModTopo -> (T.Topology -> Bool)
+--prop_appendsOneToEdgeLoops f = addXAppendsNToY f 1 T.getEdgeLoops
 
-prop_hasOneAdjacentVertex :: ModTopo -> (T.Topology -> Bool)
-prop_hasOneAdjacentVertex f =
-    addXCreatesXWithYAdjacentZ f T.getEdges 1 T.edgeVertices
+--prop_hasOneAdjacentVertex :: ModTopo -> (T.Topology -> Bool)
+--prop_hasOneAdjacentVertex f =
+    --addXCreatesXWithYAdjacentZ f T.getEdges 1 T.edgeVertices
 
-prop_hasTwoAdjacentVertices :: ModTopo -> (T.Topology -> Bool)
-prop_hasTwoAdjacentVertices f =
-    addXCreatesXWithYAdjacentZ f T.getEdges 2 T.edgeVertices
+--prop_hasTwoAdjacentVertices :: ModTopo -> (T.Topology -> Bool)
+--prop_hasTwoAdjacentVertices f =
+    --addXCreatesXWithYAdjacentZ f T.getEdges 2 T.edgeVertices
 
-prop_hasOneAdjacentFace :: ModTopo -> (T.Topology -> Bool)
-prop_hasOneAdjacentFace f =
-    addXCreatesXWithYAdjacentZ f T.getEdges 1 T.edgeFaces
+--prop_hasOneAdjacentFace :: ModTopo -> (T.Topology -> Bool)
+--prop_hasOneAdjacentFace f =
+    --addXCreatesXWithYAdjacentZ f T.getEdges 1 T.edgeFaces
 
-prop_edgeLoopPointsToNewVertex :: T.Topology -> Bool
-prop_edgeLoopPointsToNewVertex t0 =
-    let t  = T.addOpenEdgeLoop t0
-        (el, _) = Map.findMax $ T.getEdgeLoops t
-        t' = T.addEdgeToLoop el t
-        (v, _) = Map.findMax $ T.getVertices t'
-        (_, T.OpenEdgeLoop v') = Map.findMax $ T.getEdgeLoops t'
-    in v == v'
+--prop_edgeLoopPointsToNewVertex :: T.Topology -> Bool
+--prop_edgeLoopPointsToNewVertex t0 =
+    --let t  = T.addOpenEdgeLoop t0
+        --(el, _) = Map.findMax $ T.getEdgeLoops t
+        --t' = T.addEdgeToLoop el t
+        --(v, _) = Map.findMax $ T.getVertices t'
+        --(_, T.OpenEdgeLoop v') = Map.findMax $ T.getEdgeLoops t'
+    --in v == v'
+    --
