8c4471bd8e95438bb0e0f07fcb669ef58fc644df
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-05-28 21:58:22 -0400

    Uncommented failing test

--- test/TopologySpec.hs
+++ test/TopologySpec.hs
@@ -9,8 +9,8 @@ spec = do
     describe "addFreeVertex" $ do
         it "Appends one to the existing Vertices" $ do
             property (prop_appendsOneToVertices T.addVertex)
-        --it "Does not modify the Edges" $ do
-            --property (prop_doesNotModifyEdges T.addFreeVertex)
+        it "Does not modify the Edges" $ do
+            property (prop_doesNotModifyEdges T.addVertex)
         --it "Does not modify the Faces" $ do
             --property (prop_doesNotModifyFaces T.addFreeVertex)
     --describe "addRayEdge" $ do
@@ -116,23 +116,23 @@ prepXaddXAppendsNToY prepX addX n getY t0 =
         xs'   = getY t'
      in xs == take (length xs' - n) xs'
 
---addXDoesNotModifyY ::
-    --(Ord a, Eq b) =>
-        --ModTopo
-        -- -> (T.Topology -> Map.Map a b)
-        -- -> T.Topology -> Bool -- this is what is left for QuickCheck
---addXDoesNotModifyY = prepXaddXDoesNotModifyY id
+addXDoesNotModifyY ::
+    (Ord a, Eq b) =>
+        ModTopo
+         -> (T.Topology -> [T.Element])
+         -> T.Topology -> Bool -- this is what is left for QuickCheck
+addXDoesNotModifyY = prepXaddXDoesNotModifyY id
 
---prepXaddXDoesNotModifyY ::
-    --(Ord a, Eq b) =>
-        --ModTopo
-        -- -> ModTopo
-        -- -> (T.Topology -> Map.Map a b)
-        -- -> T.Topology -> Bool -- this is what is left for QuickCheck
---prepXaddXDoesNotModifyY prepX addX getY t0 =
-    --let t  = prepX t0
-        --t' = addX t
-    --in (getY t) ==  (getY t')
+prepXaddXDoesNotModifyY ::
+    (Ord a, Eq b) =>
+        ModTopo
+         -> ModTopo
+         -> (T.Topology -> [T.Element])
+         -> T.Topology -> Bool -- this is what is left for QuickCheck
+prepXaddXDoesNotModifyY prepX addX getY t0 =
+    let t  = prepX t0
+        t' = addX t
+    in (getY t) == (getY t')
 
 --addXCreatesXWithYAdjacentZ ::
     --(Ord a, Eq b, Ord c, Eq d) =>
@@ -170,8 +170,8 @@ prop_appendsOneToVertices f = addXAppendsNToY f 1 T.getVertices
 --prop_doesNotModifyVertices' :: ModTopo -> ModTopo -> (T.Topology -> Bool)
 --prop_doesNotModifyVertices' p f = prepXaddXDoesNotModifyY p f T.getVertices
 
---prop_doesNotModifyEdges :: ModTopo -> (T.Topology -> Bool)
---prop_doesNotModifyEdges f = addXDoesNotModifyY f T.getEdges
+prop_doesNotModifyEdges :: ModTopo -> (T.Topology -> Bool)
+prop_doesNotModifyEdges f = addXDoesNotModifyY f T.getEdges
 
 --prop_doesNotModifyFaces :: ModTopo -> (T.Topology -> Bool)
 --prop_doesNotModifyFaces f = addXDoesNotModifyY f T.getFaces
