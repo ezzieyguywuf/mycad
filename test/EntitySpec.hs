5d40f33cb000629ea941de9e67c74eb840522929
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-01 16:16:36 -0400

    Added failing test for Entity.addVertex

--- test/EntitySpec.hs
+++ test/EntitySpec.hs
@@ -8,12 +8,22 @@ spec = do
     describe "emptyWorkspace" $ do
         it "Creates an Entity with zero sub-components" $
             (subCount E.nullEntity) `shouldBe` 0
+    describe "addVertex" $ do
+        it "Adds a single Vertex to the Entity" $
+            (getDelta E.addVertex E.getVertices E.nullEntity) `shouldBe` 0
 
 -- ===========================================================================
 --                            Helper Functions
 -- ===========================================================================
+type EntityMod = E.Entity -> E.Entity
+type GetComponents a = E.Entity -> [a]
 
 subCount :: E.Entity a -> Int
 subCount ws = vs + es
     where vs = (length . E.getVertices) ws
           es = (length . E.getEdges) ws
+
+getDelta :: EntityMod -> GetComponents a -> E.Entity -> Int
+getDelta f g e = xs' - xs
+    where xs  = (length . f) e
+          xs' = (length . f) $ g e
