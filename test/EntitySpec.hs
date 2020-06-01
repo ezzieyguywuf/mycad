a27d3b364f49f64a692411733be8ae3c46f13ba0
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-01 19:19:29 -0400

    Explicitly state Entity type.

--- test/EntitySpec.hs
+++ test/EntitySpec.hs
@@ -3,27 +3,30 @@ module EntitySpec (spec) where
 import Test.Hspec
 import qualified Entity as E
 
+nullE :: E.Entity Int
+nullE = E.nullEntity
+
 spec :: Spec
 spec = do
     describe "emptyWorkspace" $ do
         it "Creates an Entity with zero sub-components" $
-            (subCount E.nullEntity) `shouldBe` 0
+            (subCount nullE) `shouldBe` 0
     describe "addVertex" $ do
         it "Adds a single Vertex to the Entity" $
-            (getDelta E.addVertex E.getVertices E.nullEntity) `shouldBe` 0
+            (getDelta E.addVertex E.getVertices nullE) `shouldBe` 0
 
 -- ===========================================================================
 --                            Helper Functions
 -- ===========================================================================
-type EntityMod = E.Entity -> E.Entity
-type GetComponents a = E.Entity -> [a]
+type EntityMod a = E.Entity a -> E.Entity a
+type GetComponents a b = E.Entity a -> [b]
 
 subCount :: E.Entity a -> Int
 subCount ws = vs + es
     where vs = (length . E.getVertices) ws
           es = (length . E.getEdges) ws
 
-getDelta :: EntityMod -> GetComponents a -> E.Entity -> Int
+getDelta :: EntityMod a -> GetComponents a b -> E.Entity a -> Int
 getDelta f g e = xs' - xs
-    where xs  = (length . f) e
-          xs' = (length . f) $ g e
+    where xs  = (length . g) e
+          xs' = (length . g) $ f e
