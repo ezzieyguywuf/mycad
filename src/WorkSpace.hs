008726e847b35c380c05d88f8f55d9846724763d
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-01 11:54:10 -0400

    Added empty WorkSpace module file.

---
+++ src/WorkSpace.hs
@@ -0,0 +1,14 @@
+{-|
+Module      : WorkSpace
+Description : A CAD workspace
+Copyright   : (c) Wolfgang E. Sanyer, 2020
+License     : MPL2
+Maintainer  : WolfgangESanyer@Gmail.com
+Stability   : experimental
+Portability : POSIX
+
+This module provides a workspace in which various operations can be performed to adjust
+the Geometry and Topology of a CAD model.
+-}
+module WorkSpace() where
+
