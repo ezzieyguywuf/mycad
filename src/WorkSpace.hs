653ed9d32b60e682b945bef41fa68f60df2eeb78
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-01 12:19:56 -0400

    Updated WorkSpace to pass tests.

--- src/WorkSpace.hs
+++ src/WorkSpace.hs
@@ -10,5 +10,44 @@ Portability : POSIX
 This module provides a workspace in which various operations can be performed to adjust
 the Geometry and Topology of a CAD model.
 -}
-module WorkSpace() where
+module WorkSpace
+( -- * Exported Types
+  -- | These are wrappers around Geometry and Topology data types, and also form the
+  --   "glue" that connects a given Topology to a piece of Geometry.
+  Vertex
+, Edge
+  -- * Creation
+, emptyWorkSpace
+  -- * Inspection
+, entityCount
+) where
 
+import qualified Geometry as Geo
+import qualified Topology as Topo
+
+-- ===========================================================================
+--                               Data Types
+-- ===========================================================================
+data Entity g t = Entity {getGeo :: g,
+                          getTopo :: t}
+                          deriving (Show, Eq)
+
+type Vertex = Entity (Geo.Point Int) Topo.Vertex
+type Edge   = Entity (Geo.Line Int) Topo.Edge
+
+data WorkSpace = WorkSpace { getVertices :: [Vertex]
+                           , getEdges :: [Edge]
+                           }
+
+-- ===========================================================================
+--                       Exported Free Functions
+-- ===========================================================================
+
+
+emptyWorkSpace :: WorkSpace
+emptyWorkSpace = WorkSpace [] []
+
+entityCount :: WorkSpace -> Int
+entityCount ws = vs + es
+    where vs = (length . getVertices) ws
+          es = (length . getEdges) ws
