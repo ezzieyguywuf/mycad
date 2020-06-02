74812fafd247152f06b5cd0bad3340e210e9173a
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-01 22:56:46 -0400

    Got rid of subsCount helper function

--- test/EntitySpec.hs
+++ test/EntitySpec.hs
@@ -11,7 +11,9 @@ spec :: Spec
 spec = do
     describe "nullEntity" $ do
         it "Creates an Entity with zero sub-components" $
-            (subCount nullE) `shouldBe` 0
+            let vs = (length . E.getVertices) nullE
+                es = (length . E.getEdges) nullE
+            in (vs + es) `shouldBe` 0
     describe "addVertex" $ do
         it "Adds a single Vertex to the Entity" $
             let vs = E.getVertices $ E.addVertex nullE p
@@ -21,11 +23,3 @@ spec = do
             let p = Geo.makePoint 10 20 0
                 v = last . E.getVertices $ E.addVertex nullE p
             in E.getPoint v `shouldBe` p
-
--- ===========================================================================
---                            Helper Functions
--- ===========================================================================
-subCount :: E.Entity a -> Int
-subCount ws = vs + es
-    where vs = (length . E.getVertices) ws
-          es = (length . E.getEdges) ws
