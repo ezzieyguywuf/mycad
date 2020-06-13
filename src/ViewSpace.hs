7a4a49d30d9233985bd3d6d829132044862b5370
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-13 13:34:05 -0400

    Cleaned up code a bit.

--- src/ViewSpace.hs
+++ src/ViewSpace.hs
@@ -20,38 +20,23 @@ data Camera = LookAt {
 _pprintV3 :: RealFloat a => V3 a -> String
 _pprintV3 v = (foldMap (showFFloat (Just 3)) v) ", "
 
--- | Rotates the camera around the origin
---
---   Imagine looking straight down at a sphere and pinching the point closest
---   to you. Now move your fingers up/down left/right. This will cause the
---   sphere to rotate around its center. That's what this function does.
---
---   The maximum you'll be able to rotate the camera in such a manner is
---   90-degrees in any direction. This is intended to be called incrementally,
---   say when tracking a mouse pointer
+-- | Rotates the camera.
 rotateCameraNudge :: IORef Camera
                   -> Float         -- ^ dx
                   -> Float         -- ^ dy
                   -> IO ()
-rotateCameraNudge ioCam dx dy = do
+rotateCameraNudge ioCam yaw pitch = do
     (LookAt loc up dir) <- readIORef ioCam
     let p1@(V3 x1 y1 z1) = normalize loc
         p2 = normalize $ rotate (pitchRot * yawRot) p1
         right = (loc - dir) `cross` up
-        yawRot = axisAngle up dx
-        pitchRot = axisAngle right (-dy)
+        yawRot = axisAngle up yaw
+        pitchRot = axisAngle right (-pitch)
         axis = p1 `cross` p2
         theta = acos (p1 `dot` p2)
         rot = axisAngle axis theta
         loc' = rotate rot loc
         up'  = rotate rot up
-    putStrLn $ "P1 = " <> (show p1)
-    putStrLn $ "    P2 = " <> (show p2)
-    putStrLn $ "    dx = " <> (show dx) <> ", dy = " <> (show dy)
-    putStrLn $ "    axis= " <> (show axis)
-    putStrLn $ "    theta= " <> (show theta)
-    putStrLn $ "    up'= " <> (show up')
-    putStrLn $ "    loc' = " <> (show loc')
         
     writeIORef ioCam (LookAt loc' up' dir)
 
@@ -62,32 +47,3 @@ zoomCamera ioCam amt = do
         rad' = rad - amt
         loc' = lerp (rad' / rad) loc dir
     writeIORef ioCam (LookAt loc' up dir)
-
-
---getZ :: Float -> Float -> Float -> Float
---getZ rad x y = sqrt (rad**2 - x**2 - y**2)
---moveCamera :: IORef Camera -> Float -> Float -> IO ()
---moveCamera ioCam yaw pitch = do
-    --cam <- readIORef ioCam
-    --let pos =  getPosition cam
-        --dir =  getDirection cam
-        --up  =  getUp cam
-        --pitchAxis = (pos - dir) `cross` up
-        --yawRot = axisAngle up yaw
-        --pitchRot = axisAngle pitchAxis pitch
-        --totalRot = (pitchRot * yawRot)
-        --pos'   = rotate totalRot pos
-        --up'    = rotate pitchRot up
-        --newCam = LookAt { getPosition = pos'
-                        --, getUp = up'
-                        --, getDirection = getDirection cam
-                        --, getLastPitchAxis = pitchAxis}
-    ----putStrLn $ "pos' = " <> pprintV3 pos'
-    ----putStrLn $ "    pos = " <> pprintV3 pos
-    ----putStrLn $ "    dir = " <> pprintV3 dir
-    ----putStrLn $ "    up = " <> pprintV3 up
-    ----putStrLn $ "    up' = " <> pprintV3 up'
-    ----putStrLn $ "    pitchAxis = " <> pprintV3 pitchAxis
-    ----putStrLn $ "    radians = " <> show pitch
-    ----putStrLn $ "    totalRot =" <> show totalRot
-    --writeIORef ioCam newCam
