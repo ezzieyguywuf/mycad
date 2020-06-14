0812815ad2eb4f5d6050b823f9c2cd8e78262703
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-13 22:13:58 -0400

    Stop using texture.

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -7,7 +7,5 @@ uniform sampler2D texture1;
 
 void main()
 {
-    FragColor = mix(texture(texture0, TexCoord),
-                    texture(texture1, TexCoord),
-                    0.2);
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
