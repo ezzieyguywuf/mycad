a4c7338d3e843849db5010e9a1545354eb63178f
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-16 19:26:20 -0400

    added makeLine function. Currently not used.

--- GUI/src/VertexData.hs
+++ GUI/src/VertexData.hs
@@ -3,13 +3,47 @@ module VertexData
   line
 , line'
 , cube
+, makeLine
 ) where
 
 -- gl, all types and funcs here will already start with "gl"
 import Linear.V3
 import Linear.V4
 import Linear.Quaternion
+import Linear.Metric
+import Linear.Vector
 import GraphicData
+
+makeLine :: V3 Float      -- ^ from
+            -> V3 Float   -- ^ to
+            -> ObjectData -- ^ drawable object
+makeLine p1 p2 = ObjectData eData placementData
+    where eData = makeElementData' line lineIndices
+          width = 3
+          normal = normalize (-1 *^ (p2 - p1))
+          p1    = (p1 + (width *^ normal))
+          p2    = (p1 - (width *^ normal))
+          p3    = (p2 + (width *^ normal))
+          p4    = (p2 - (width *^ normal))
+          line  = [ [Position p1, Color (V4 1.0 0.5 0.2 1)]
+                  , [Position p2, Color (V4 1.0 0.5 0.2 1)]
+                  , [Position p3, Color (V4 1.0 0.5 0.2 1)]
+                  , [Position p3, Color (V4 0.0 0.5 0.2 1)]
+                  , [Position p2, Color (V4 0.0 0.5 0.2 1)]
+                  , [Position p4, Color (V4 0.0 0.5 0.2 1)]
+                  ]
+          lineIndices =
+              [
+                0, 1, 2
+              , 3, 4, 5
+              ]
+          placementData =
+              [
+                PlacementData { placementRotation=(axisAngle (V3 0 0 0) 0)
+                              , placementTranslation=(V3 0 0 0)}
+              , PlacementData { placementRotation=(axisAngle (V3 0 0 1) (-pi/4))
+                              , placementTranslation=(V3 15 0 0)}
+              ]
 
 line :: ObjectData
 line = ObjectData eData placementData
