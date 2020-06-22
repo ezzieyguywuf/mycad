c4727ed738c3bfe16e338b2e7248f322e7370b9b
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-22 14:48:26 -0400

    Added documentation and re-arranged main.

--- HaskellCAD/app/Main.hs
+++ HaskellCAD/app/Main.hs
@@ -3,28 +3,11 @@ import Entity
 import Commands
 import Control.Monad.State
 
--- Evaluation : handle each line user inputs
-evaluateCommand :: String -> Entity a -> InputT IO (Entity a)
-evaluateCommand input entity = do
-    let (ret, entity') = runState (tryCommand input) entity
-    outputStrLn ret
-    pure entity'
-
-completer :: String -> IO [Completion]
-completer s = pure $ map makeComplete (filterKnown s)
-    where makeComplete :: String -> Completion
-          makeComplete s = Completion s s False
-
-settings :: Settings IO
-settings = Settings {
-                      complete = completeWord Nothing [' ', '\t'] completer
-                    , historyFile = Nothing
-                    , autoAddHistory = True
-                    }
-
+-- | Run Haskeline
 main :: IO ()
 main = runInputT settings (loop nullEntity)
 
+-- | Keep getting input from User until quit
 loop :: Entity a -> InputT IO ()
 loop entity = do
     minput <- getInputLine ">>> "
@@ -32,3 +15,24 @@ loop entity = do
         Nothing -> pure ()
         Just "quit" -> pure ()
         Just input -> evaluateCommand input entity >>= loop
+
+-- | Evaluation : handle each line user inputs
+evaluateCommand :: String -> Entity a -> InputT IO (Entity a)
+evaluateCommand input entity = do
+    let (ret, entity') = runState (tryCommand input) entity
+    outputStrLn ret
+    pure entity'
+
+-- | Provides tab-completion to Haskeline's InputT
+completer :: String -> IO [Completion]
+completer s = pure $ map makeComplete (filterKnown s)
+    where makeComplete :: String -> Completion
+          makeComplete s = Completion s s False
+
+-- | Provides setting information to InputT
+settings :: Settings IO
+settings = Settings {
+                      complete = completeWord Nothing [' ', '\t'] completer
+                    , historyFile = Nothing
+                    , autoAddHistory = True
+                    }
