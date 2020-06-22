15888c4075412fd21a3e2adcd910db1d28850699
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-22 17:46:16 -0400

    Added Runnable data type

--- HaskellCAD/src/Commands.hs
+++ HaskellCAD/src/Commands.hs
@@ -1,6 +1,7 @@
 module Commands
 (
-  Command
+  Command(..)
+, Runnable(..)
 , parseCommand
 , execCommand
 , filterKnown
@@ -17,22 +18,25 @@ data Command = Add
              | Help
              deriving (Enum, Bounded, Show)
 
+-- | This matches a Command with a list of one or more arguments to be run using "runCommand"
+data Runnable = Runnable Command Args
+
 -- | Try to parse a String into a Command
-parseCommand :: String -> Either String (Command, Args)
+parseCommand :: String -> Either String Runnable
 parseCommand input =
     case parsed of
         Nothing  -> Left $ "The command \"" <> input
                             <> "\" is not understood. Please type help for more help"
-        Just val -> Right val
+        Just (command, args) -> Right $ Runnable command args
     where parsed = do
           (commandName, args) <- uncons (words input)
           command <- Map.lookup commandName knownCommands
           pure (command, args)
 
 -- | Either execute the command and return the mutated Entity, or return an error message
-execCommand :: (Command, Args) -> Either String (Entity a)
-execCommand (Help, _)    = Left $ "These are the known commands: " <> (show commands)
-execCommand (command, _) = Left $ "The command " <> (show command) <> " has not yet been implemented"
+execCommand :: Runnable -> Either String (Entity a)
+execCommand (Runnable Help _)    = Left $ "These are the known commands: " <> (show commands)
+execCommand (Runnable command _) = Left $ "The command " <> (show command) <> " has not yet been implemented"
 
 
 -- | Returns a list of commands which partially match the provided String
