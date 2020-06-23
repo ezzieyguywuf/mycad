2ae143203fe12230960fd7c0d9a409b8563bde22
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-22 20:36:03 -0400

    Updated parseCommand to provide alternatives when available.

--- HaskellCAD/src/Commands.hs
+++ HaskellCAD/src/Commands.hs
@@ -8,7 +8,7 @@ module Commands
 )where
 
 import qualified Data.Map as Map
-import Data.List (isPrefixOf, uncons)
+import Data.List (isPrefixOf, uncons, intersperse)
 import Entity
 
 -- | These are commands that can be executed upon an Entity
@@ -23,15 +23,14 @@ data Runnable = Runnable Command Args
 
 -- | Try to parse a String into a Command
 parseCommand :: String -> Either String Runnable
-parseCommand input =
-    case parsed of
-        Nothing  -> Left $ "The command \"" <> input
-                            <> "\" is not understood. Please type help for more help"
-        Just (command, args) -> Right $ Runnable command args
-    where parsed = do
-          (commandName, args) <- uncons (words input)
-          command <- Map.lookup commandName knownCommands
-          pure (command, args)
+parseCommand input = do
+    (cmd, args) <- checkValid input
+    matches     <- filterKnown' cmd
+    case compare (length matches) 1 of
+        EQ -> Right $ Runnable cmd' args
+                  where Just cmd' = Map.lookup (head matches) knownCommands
+        LT -> Left "checkValid function seems to have failed"
+        GT -> Left $ "Perhaps you meant one of these: " <> (concat $ intersperse ", " matches)
 
 -- | Either execute the command and return the mutated Entity, or return an error message
 execCommand :: Runnable -> Either String (Entity a)
@@ -59,3 +58,16 @@ knownCommands = Map.fromList
     , ("connect", Connect)
     , ("help", Help)
     ]
+
+-- | Checks whether a user's input is valid or not
+checkValid :: String -> Either String (String, [String])
+checkValid s = case uncons (words s) of
+                   Nothing -> Left "You must provide at least one character of input"
+                   Just split -> Right split
+
+-- | Either returns a list of commands with partially match the provided String or an error
+filterKnown' :: String -> Either String [String]
+filterKnown' s = case filterKnown s of
+                    []  -> Left $ "The command \"" <> s
+                                  <> "\" is not understood. Please type help for more help"
+                    xs  -> Right xs
