6321d51ea8e620b86225379d5af44e5ae4bf5f5e
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-24 12:16:52 -0400

    Updated runCommand to accept an Entity

--- HaskellCAD/src/CommandRunner.hs
+++ HaskellCAD/src/CommandRunner.hs
@@ -11,10 +11,11 @@ import Entity
 
 -- | Either execute the command and return the mutated Entity, or return an error message
 runCommand :: Entity a -> Runnable -> Either String (Entity a)
-runCommand entity (Runnable Help [])    = Left $ runHelp ""
-runCommand entity (Runnable Help (arg:_))    = Left $ runHelp arg
-runCommand entity (Runnable Add (arg:_))  = runAdd arg
-runCommand entity (Runnable command _) = Left $ "The command " <> (show command) <> " has not yet been implemented"
+runCommand entity (Runnable Help [])      = Left $ runHelp ""
+runCommand entity (Runnable Help (arg:_)) = Left $ runHelp arg
+runCommand entity (Runnable Add (arg:_))  = runAdd entity arg
+runCommand entity (Runnable command _)    =
+    Left $ "The command " <> (show command) <> " has not yet been implemented"
 
 -- ===========================================================================
 --                   Private Free Functions and Data Type
@@ -25,9 +26,9 @@ runHelp _ = "These are the known commands:\n"
             <> "    " <> (show commands) <> "\n"
             <> "Try help <command> for more information."
 
-runAdd :: String -> Either String (Entity a)
-runAdd "" = Left $ runHelp "add"
-runAdd arg
+runAdd :: Entity a -> String -> Either String (Entity a)
+runAdd _ "" = Left $ runHelp "add"
+runAdd entity arg
   | arg == "vertex" = Left "Adding vertex"
   | arg == "edge"   = Left "Adding edge"
   | otherwise = Left $ runHelp "add"
