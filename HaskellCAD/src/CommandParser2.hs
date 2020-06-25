6aad4da7d21aff92d7e77326711ef93cba586f35
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-25 16:43:56 -0400

    Revert back to before ParsecT

--- HaskellCAD/src/CommandParser2.hs
+++ HaskellCAD/src/CommandParser2.hs
@@ -11,17 +11,25 @@ import Data.Void
 
 import Linear.V3
 
-type Parser = ParsecT Void Text Maybe
+type Parser = Parsec Void Text
 
+-- | This is a command that the user can request
+data Command a = Add (Item a)
+               | Help
+               deriving (Show)
 
---maybeCommand :: Parser Text
---maybeCommand = do
-    --fmap Just $ choice [ string "add"
-                                 --, string "help"
-                                 --]
-    -- <|> pure Nothing
+-- | This is something that the user can Add
+data Item a = Vertex (Geo.Point a)
+              deriving (Show)
 
-maybeInteger :: Parser Int
+maybeCommand :: Parser (Maybe Text)
+maybeCommand = do
+    fmap Just $ choice [ string "add"
+                       , string "help"
+                       ]
+    <|> pure Nothing
+
+maybeInteger :: Parser (Maybe Int)
 maybeInteger = do
     (fmap (Just . read) (some digitChar))
     <|> pure Nothing
@@ -36,11 +44,3 @@ maybeInteger = do
     --pure $ Just (V3 x y z)
     -- <|> pure Nothing
 
--- | This is a command that the user can request
-data Command a = Add (Item a)
-               | Help
-               deriving (Show)
-
--- | This is something that the user can Add
-data Item a = Vertex (Geo.Point a)
-              deriving (Show)
