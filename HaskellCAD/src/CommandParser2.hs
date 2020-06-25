9b8294d793d7135f77e722e43a49572ee1897396
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-25 11:37:37 -0400

    Trying to make a Point parser.

--- HaskellCAD/src/CommandParser2.hs
+++ HaskellCAD/src/CommandParser2.hs
@@ -1,8 +1,10 @@
 module CommandParser2 where
 
 import qualified Geometry as Geo
+import Data.Char (isDigit)
 
 import Text.ParserCombinators.ReadP
+import Linear.V3
 
 -- | This is a command that the user can request
 data Command a = Add (Item a)
@@ -18,11 +20,35 @@ parseCommand = choice [ string "add"
                       , string "help"
                       ]
 
-maybeCommand :: ReadP (Maybe (Command a))
+parseInteger :: ReadP Int
+parseInteger = do
+    x <- munch1 isDigit
+    pure $ read x
+
+parsePoint' :: ReadP (Geo.Point Int)
+parsePoint' = do
+    x <- parseInteger
+    skipSpaces
+    y <- parseInteger
+    skipSpaces
+    z <- parseInteger
+    pure $ V3 x y z
+
+--maybePoint :: ReadP (Maybe (Geo.Point Int))
+--maybePoint = do
+    --val <- parsePoint'
+    --case val of
+        --[]    -> pure Nothing
+        --point -> pure $ Just point
+
+maybeCommand :: Num a => ReadP (Maybe (Command a))
 maybeCommand = do
     parsed <- parseCommand
     case parsed of
        ""     -> pure $ Nothing
        "help" -> pure $ Just Help
-       --"add"  -> parsePoint >>= (Just Add)
+       "add"  -> pure $ parsePoint >>= (\x -> Just (Add x))
        _      -> pure $ Nothing
+
+parsePoint :: Num a => (Maybe (Item a))
+parsePoint = Just (Vertex (V3 10 10 10))
