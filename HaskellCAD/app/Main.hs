d680aa8e1114563d7069cd1e6a7ed1c11004d4d9
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-26 12:56:04 -0400

    Added documentation to Main module.

--- HaskellCAD/app/Main.hs
+++ HaskellCAD/app/Main.hs
@@ -1,3 +1,19 @@
+{-|
+Module      : Main
+Description : The main executable for MyCAD
+Copyright   : (c) Wolfgang E. Sanyer, 2020
+License     : MPL2
+Maintainer  : WolfgangESanyer@Gmail.com
+Stability   : experimental
+Portability : POSIX
+
+This is the main entry-point for the MyCAD application. It provides a
+REPL-style interface using the Haskell package Haskeline to achieve the CLI
+interface.
+
+Haskeline itself seems pretty safe as a dependency - I've heard that even the
+ubiquitous ghci using Haskeline behind-the-scene somewhere.
+-}
 module Main (main) where
 
 import Control.Monad.Except (runExcept)
