15f7bd480ae83d95b5b39ee5854ed3a2d49f829e
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-25 23:17:21 -0400

    Added brief intro message.

--- HaskellCAD/app/Main2.hs
+++ HaskellCAD/app/Main2.hs
@@ -12,6 +12,7 @@ main = runInputT settings mainLoop
 -- | Entry point for main loop
 mainLoop :: InputT IO ()
 mainLoop = do
+    outputStrLn "Welcome to mycad. [Ctrl-d] to exit."
     input <- getInputLine "mycad> "
     maybe (outputStrLn "exiting.") loopAgain input
 
