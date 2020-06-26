6ebc0f1ae7deb7ddad7dcec515e32ed4971f9554
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-25 21:11:39 -0400

    Formatting change.

--- HaskellCAD/src/CommandParser3.hs
+++ HaskellCAD/src/CommandParser3.hs
@@ -10,17 +10,28 @@ data ParseError = EmptyInput
 data Command = Help
                deriving (Show)
 
+data Argument = String String
+                deriving (Show)
+
 isStatement :: String -> Either ParseError [String]
-isStatement input = case words input of
-                       []    -> Left EmptyInput
-                       split -> Right split
+isStatement input =
+    case words input of
+       []    -> Left EmptyInput
+       split -> Right split
 
 isCommand :: [String] -> Either ParseError (Command, [String])
-isCommand input = case uncons input of
-                      Nothing    -> Left InvalidInput
-                      Just (cmd,args) -> knownCommand cmd >>= \cmd' -> Right (cmd', args)
+isCommand input =
+    case uncons input of
+       Nothing    -> Left InvalidInput
+       Just (cmd,args) -> knownCommand cmd >>= \cmd' -> Right (cmd', args)
+
+parseArgs :: (Command, [String]) -> Either ParseError (Command, [Argument])
+parseArgs (cmd, args) =
+    case cmd of
+       Help -> []
 
 knownCommand :: String -> Either ParseError Command
-knownCommand string = case string of
-                          "help" -> Right Help
-                          _      -> Left UnknownCommand
+knownCommand string =
+    case string of
+       "help" -> Right Help
+       _      -> Left UnknownCommand
