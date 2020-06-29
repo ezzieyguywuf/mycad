931e5253c301d845a6836f5ff85aa886ebeb4a4d
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-06-28 23:13:16 -0400

    Deleted rotating line.

--- MyCAD/GUI/Main.hs
+++ MyCAD/GUI/Main.hs
@@ -17,9 +17,7 @@ import Graphics.GL.Core33
 
 -- For Linear algebra...but really, like vectors and matrices and quaternions
 import Linear.V3
-import Linear.V4
 import Linear.Projection
-import Linear.Quaternion
 
 import Data.IORef
 
@@ -75,19 +73,6 @@ act camera window = do
                 drawObject cubeDrawer
                 drawObject lineDrawer'
                 drawObject circleDrawer
-
-                -- Draw the rotating line
-                time <- maybe 0 realToFrac <$> GLFW.getTime
-                let p0 = V3 (-15) 15 0
-                    p2 = V3 (-15) (-15) 0
-                    axis = axisAngle (V3 0 0 1) (pi * ((sin time) + 1))
-                    p1' = Linear.Quaternion.rotate (axis) (p0 - p2)
-                    p1 = p1' + p2
-                    col1 = V4 0.5 1.0 0.2 1.0
-                cam <- readIORef camera
-                lineDrawer2 <- makeObjectDrawer lineShader (makeLine' p1 p2 col1)
-
-                drawObject lineDrawer2
 
                 rotateCameraNudge camera (-0.005) 0
 
