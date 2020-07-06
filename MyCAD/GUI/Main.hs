7443aa0adbcb97ca8fbd2d3599ae46c9cfb71518
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-06 15:00:55 -0400

    Eliminated another flip

--- MyCAD/GUI/Main.hs
+++ MyCAD/GUI/Main.hs
@@ -41,17 +41,17 @@ act window = do
         -- >>= (flip addObject (makeLine (V3 10 10 10) (V3 10 20 10)))
         -- cube below
         addObject renderer  (makeLine (V3 (-10) (-10) (-10))    (V3 10  (-10)  (-10)))
-         >>= flip addObject (makeLine (V3 10  (-10)  (-10)) (V3 10 10  (-10)))
-         >>= flip addObject (makeLine (V3 10 10  (-10)) (V3  (-10) 10  (-10)))
-         >>= flip addObject (makeLine (V3  (-10) 10  (-10)) (V3  (-10)  (-10) (-10)))
-         >>= flip addObject (makeLine (V3  (-10)  (-10) (-10)) (V3  (-10) (-10) 10))
-         >>= flip addObject (makeLine (V3 10 (-10) (-10))  (V3 10 (-10) 10))
-         >>= flip addObject (makeLine (V3 10 10 (-10))  (V3 10 10 10))
-         >>= flip addObject (makeLine (V3  (-10) 10 (-10))  (V3  (-10) 10 10))
-         >>= flip addObject (makeLine (V3  (-10)  (-10) 10) (V3 10  (-10) 10))
-         >>= flip addObject (makeLine (V3 10 (-10) 10) (V3 10 10 10))
-         >>= flip addObject (makeLine (V3 10 10 10) (V3  (-10) 10 10))
-         >>= flip addObject (makeLine (V3 (-10) 10 10) (V3  (-10) (-10) 10))
+         >>= (`addObject` (makeLine (V3 10  (-10)  (-10)) (V3 10 10  (-10))))
+         >>= (`addObject` (makeLine (V3 10 10  (-10)) (V3  (-10) 10  (-10))))
+         >>= (`addObject` (makeLine (V3  (-10) 10  (-10)) (V3  (-10)  (-10) (-10))))
+         >>= (`addObject` (makeLine (V3  (-10)  (-10) (-10)) (V3  (-10) (-10) 10)))
+         >>= (`addObject` (makeLine (V3 10 (-10) (-10))  (V3 10 (-10) 10)))
+         >>= (`addObject` (makeLine (V3 10 10 (-10))  (V3 10 10 10)))
+         >>= (`addObject` (makeLine (V3  (-10) 10 (-10))  (V3  (-10) 10 10)))
+         >>= (`addObject` (makeLine (V3  (-10)  (-10) 10) (V3 10  (-10) 10)))
+         >>= (`addObject` (makeLine (V3 10 (-10) 10) (V3 10 10 10)))
+         >>= (`addObject` (makeLine (V3 10 10 10) (V3  (-10) 10 10)))
+         >>= (`addObject` (makeLine (V3 (-10) 10 10) (V3  (-10) (-10) 10)))
 
     -- enter our main loop
     loop window renderer'
