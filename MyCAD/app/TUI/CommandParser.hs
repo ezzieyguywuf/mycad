0d6f95fb6429755964c8feeff866aab3c356b0b4
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-19 13:06:28 -0400

    Renamed CommandParser2 to CommandParser. Still need to refactor CommandRunner.

--- MyCAD/app/TUI/CommandParser.hs
+++ MyCAD/app/TUI/CommandParser.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 {-|
-Module      : CommandParser
+Module      : CommandParser2
 Description : Parses string commands to be used in the MyCAD application
 Copyright   : (c) Wolfgang E. Sanyer, 2020
 License     : MPL2
@@ -10,122 +12,164 @@ Portability : POSIX
 This module provides a string-parser which produces a "Command" to be used by
 the MyCAD application.
 
-This parser in somewhat naïve, and is likely to be updated to use megaparsec at
-some point in the future.
-
-As it stands, it uses the Except monad to handle errors, and typically falls
-back to a "Help" command when things go wrong.
+This second attempt uses megaparsec for the parsing, rather than rolling our
+own parser as we did at first
 -}
-module TUI.CommandParser
+module TUI.CommandParser2
 (
--- * Data Types
-  Command (..)
-, Action  (..)
--- * Exported Functions
-, parseInput
-, commandCompletions
+  parseInput
 )where
 
-import Control.Monad.Except (throwError, catchError)
-import qualified Data.Map as Map
+-- Base
+import Data.Void (Void)
+import Control.Applicative ((<|>), optional, empty, some)
+import Data.Map (Map, fromList, assocs)
 
-import Data.List (uncons, isPrefixOf)
-import qualified Geometry as Geo
-import Data.Text (Text, pack, unpack, words, strip, intercalate)
+-- Third-Party
+import Data.Text (Text, pack)
 import Data.Text.Read (rational)
-import Linear.V3
+import Text.Megaparsec ((<?>), Parsec, parse, eof, choice, try)
+import Text.Megaparsec.Char (string, space1, char, digitChar)
+import Text.Megaparsec.Error (ParseErrorBundle)
+import qualified Text.Megaparsec.Char.Lexer as Lexer
+import Linear.V3 (V3(V3))
 
-import TUI.Errors (Error, MyError(..))
+-- Internal
+import Geometry (Point)
 
--- | A "Command" includes all the information necessary to execute an "Action"
-data Command a = Help (Maybe Action)
+type Parser = Parsec Void Text
+type ParseError = ParseErrorBundle Text Void
+
+-- | A "Command" is an action that can be performed by the user.
+--
+--   This is parametrized over the type of Point to use
+data Command a = Help (Maybe (Command a))
                | Quit
-               | AddVertex (Geo.Point a)
                | Show
-                 deriving (Show, Eq)
+               | Add (AddToken a)
+                 deriving (Show)
 
--- | An Action that can be performed.
-data Action = GetHelp
-            | QuitProgram
-            | MakeVertex
-            | ShowEntity
-              deriving (Show, Eq)
+-- | Something that can be added
+data AddToken a = Vertex (Point a) deriving (Show)
 
--- | This maps a string to each of our Action. This is not exported, but it's useful to have here.
-actionMap :: Map.Map Text Action
-actionMap = Map.fromList
-    [ (pack "help", GetHelp)
-    , (pack "quit", QuitProgram)
-    , (pack "addVertex", MakeVertex)
-    , (pack "show", ShowEntity)
+-- | This will run our parser on the given input, generating a "Command"
+parseInput :: Fractional a => Text -> Either ParseError (Command a)
+parseInput = parse startParsing ""
+
+-- | This will parse an abritrary line of input from the User.
+--
+--   Note that this will only parse a single line, which must issue some
+--   "Command"
+startParsing :: Fractional a => Parser (Command a)
+startParsing = spaceConsumer *> lexeme parseCommand <* eof
+
+-------------------------------------------------------------------------------
+--                      Internal stuff
+-------------------------------------------------------------------------------
+knownCommands :: Fractional a => Map Text (Parser (Command a))
+knownCommands = fromList
+    [ ("help", parseHelpArgs)
+    , ("quit", pure Quit)
+    , ("show", pure Show)
+    , ("add" , parseAddArgs)
     ]
 
--- | The input to this function is expected to be the raw input from the User.
+-- | Try to parse each key in the "knownCommands" "Data.Map"
 --
---   This function provides error-handling using the "Except" monad.
-parseInput :: Fractional a => String -> Error (Command a)
-parseInput string = parseStatement (pack string) >>= parseAction >>= parseCommand
+--   If the parse succeeds, it will return the accompanying "Command" in the
+--   value associated with said key.
+parseCommand :: Fractional a => Parser (Command a)
+parseCommand =
+        choice (fmap checkCommand (assocs knownCommands))
+    <?> "valid command"
 
--- | Given some string, determines if this partially matches any of our known "Command".
---
---   There's an opportunity to allow for fuzzy matching, but that's not
---   currently implemented. As it stands, the list of "String" that are
---   returned are suitable to use, say, in Haskelines tab-completion
-commandCompletions :: String -> [String]
-commandCompletions string = filter (isPrefixOf string) knownCommands
-    where knownCommands = map unpack $ Map.keys actionMap
+-- | This parses any arguments to the \"help\" command
+parseHelpArgs :: Fractional a => Parser (Command a)
+parseHelpArgs =
+    try
+        (do word "help"
+            pure (Help (Just (Help Nothing))))
+    <|> do cmd <- optional parseCommand
+           pure (Help cmd)
 
--- ===========================================================================
---                     Parsers - these are not exported
--- ===========================================================================
-parseStatement :: Text -> Error [Text]
-parseStatement input =
-    case Data.Text.words input of
-        []    -> throwError EmptyInput
-        split -> pure split
+-- | This parses any arguments to the \"add\" command
+parseAddArgs :: Fractional a => Parser (Command a)
+parseAddArgs = do
+    word "vertex"
+    point <- parsePoint
+    pure $ Add (Vertex point)
 
-parseAction :: [Text] -> Error (Action, [Text])
-parseAction input =
-    case uncons input of
-        Nothing    -> throwError InvalidInput
-        Just (cmd,args) -> case Map.lookup cmd actionMap of
-                               Just action -> pure (action, args)
-                               Nothing     -> throwError UnknownAction
-
-parseCommand :: Fractional a => (Action, [Text]) -> Error (Command a)
-parseCommand (cmd, args) =
-    case cmd of
-        GetHelp     -> parseHelpArgs args
-        QuitProgram -> pure Quit
-        MakeVertex  -> parseAddVertexArgs args
-        ShowEntity  -> pure Show
-
-parseNumber :: Fractional a => Text -> Error (a, Text)
-parseNumber text = do
-    case rational (strip text) of
-        Left _    -> throwError NumberParseError
-        Right val -> pure val
-
-parsePoint :: Fractional a => Text -> Error (Geo.Point a)
-parsePoint text = do
-    (x, t0) <- parseNumber text
-    (y, t1) <- parseNumber t0
-    (z, t2) <- parseNumber t1
+-- | This parses a 3-dimensional point x y z
+parsePoint :: Fractional a => Parser (Point a)
+parsePoint = do
+    x <- number
+    y <- number
+    z <- number
     pure (V3 x y z)
-    `catchError` \_ -> (throwError PointParseError)
 
--- ===========================================================================
---                           Argument  Parsers
--- ===========================================================================
-parseHelpArgs :: [Text] -> Error (Command a)
-parseHelpArgs args = do
-    (action, _) <- parseAction args
-    pure $ Help (Just action)
-    `catchError` \_ -> (pure $ Help Nothing)
+-- | Tries to parse (String, Parser a) pair
+checkCommand :: (Text, Parser (Command a)) -> Parser (Command a)
+checkCommand (text, parser) = word text >> parser
 
-parseAddVertexArgs :: Fractional a => [Text] -> Error (Command a)
-parseAddVertexArgs args = do
-    let args' = intercalate (pack " ") args
-    point <- parsePoint args'
-    pure $ AddVertex point
-    `catchError` \_ -> (pure $ Help (Just MakeVertex))
+-- | This will....consume space
+spaceConsumer :: Parser ()
+spaceConsumer = Lexer.space space1 empty empty
+
+-- | Use the given parser to parse a lexeme, consuming any space after
+lexeme :: Parser a -> Parser a
+lexeme = Lexer.lexeme spaceConsumer
+
+-- | Use to parse a single word
+word :: Text -> Parser Text
+word = lexeme . string
+
+-- | Use to parse an integer
+integer :: Parser String
+integer = some digitChar
+
+-- | A float with a trailing decimal
+--
+--   We add a zero after the decimal, in order to make parsing it easier
+floatTrailing :: Parser String
+floatTrailing = do
+    val <- integer
+    char '.'
+    pure $ val <> ".0"
+
+-- | A float with a leading decimal
+--
+--   We add a zero before the decimal, in order to make parsing it easier
+floatLeading :: Parser String
+floatLeading = do
+    char '.'
+    val <- integer
+    pure $ "0." <> val
+
+-- | A float with numbers both before and after the decimal
+floatBoth :: Parser String
+floatBoth = do
+    before <- integer
+    char '.'
+    after <- integer
+    pure $ before <> "." <> after
+
+-- | Use to parse a float
+--
+--   It can be input as:
+--      1. a standalone integer, e.g. "1234"
+--      2. a number with a trailing decimal, e.g. "1234."
+--      3. a number with a leading decimal, e.g. ".1234"
+--      4. an arbitrary decimal value "12.34"
+float :: Parser String
+float = try floatLeading
+    <|> try floatTrailing
+    <|> try floatBoth
+    <|> integer
+
+-- | Use to parse a number
+number :: Fractional a => Parser a
+number = lexeme $ do
+    text <- float
+    case rational . pack $ text of
+        Left err       -> fail err
+        Right (val, _) -> pure val
