b5d8fbfe77ef79efb9e18c708883e9d33fe2ee36
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-22 16:39:49 -0400

    Added failing test for addEdge

--- Topology/test/TopologySpec.hs
+++ Topology/test/TopologySpec.hs
@@ -7,11 +7,16 @@ import Control.Monad.State (execState)
 
 spec :: Spec
 spec = do
-    describe "addFreeVertex" $ do
-        it "Is inversed by removeVertex, resulting in original state" $
+    describe "addFreeVertex" $
+        it "Is inversed by removeVertex" $
             property (prop_stateIdentity (addFreeVertex >>= removeVertex))
-        --it "Adds a single Vertex to the Topology" $
-            --property (prop_addsOneVertex addFreeVertex)
+    describe "addEdge" $
+        it "Is inversed by removeEdge" $ do
+            let run = do v1 <- addFreeVertex
+                         v2 <- addFreeVertex
+                         addEdge v1 v2
+            property (prop_stateIdentity run)
+
 
 -- ===========================================================================
 --                            Properties
@@ -20,19 +25,12 @@ prop_stateIdentity :: TopoState a -> TestTopology -> Bool
 prop_stateIdentity run testTopology = topology == execState run topology
     where topology = unTestTopology testTopology
 
---prop_addsOneVertex :: TopoState a -> TestTopology -> Bool
---prop_addsOneVertex = deltaOfXisY vertices 1
-
---deltaOfXIsY :: TopoState a -> TestTopology -> TopoGetter a -> Int -> Bool
-
 -- ===========================================================================
 --                            Helper Functions
 -- ===========================================================================
 -- | In order to fix an orphaned instance warning, we'll wrap this in newtype
 --   before defining a new instance
 newtype TestTopology = TestTopology {unTestTopology :: Topology} deriving (Show)
-
---type TopoGetter a = TopoState [a]
 
 -- | This will generate a random Topology
 instance Arbitrary TestTopology where
