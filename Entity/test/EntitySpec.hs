45f3ebdca77c29ec45af2605176953ad9d6afbf0
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-23 10:54:24 -0400

    Updated EntitySpec to change call signature for addEdge

--- Entity/test/EntitySpec.hs
+++ Entity/test/EntitySpec.hs
@@ -19,19 +19,8 @@ spec = do
         it "allows Vertex to be retrieved using Geometry" $
             vertex `shouldSatisfy` (`elem` getVertex entity point)
     describe "addEdge" $ do
-        let ((_, _, edge), entity) = runState estate nullE
-            p1 = V3 10 10 10
-            p2 = V3 20 20 20
-            estate = do
-                 _v1   <- addVertex p1
-                 _v2   <- addVertex p2
-                 _edge <- addEdge _v1 _v2
-                 pure (_v1, _v2, _edge)
-        it "Adds a single Edge to the Entity" $
-            (length $ getEdges entity) `shouldBe` 1
-        it "Does not modify the number of Vertex in the Entity" $
-            (length $ getVertices entity) `shouldBe` 2
-        it "Creates a line from v1 to v2" $ do
+        let (edge, entity) = runState (addEdge p1 p2) nullE
+        it "Creates a line from p1 to p2" $ do
             let line = Geo.makeLine p1 p2
             getCurve entity edge `shouldBe` Just line
     --describe "oppositeVertex" $ do
