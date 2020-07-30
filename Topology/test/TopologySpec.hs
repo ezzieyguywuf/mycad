6e573303e8443204632de74f2a7f75e797b61528
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-30 10:56:08 -0400

    Renamed a bunch of test properties.

--- Topology/test/TopologySpec.hs
+++ Topology/test/TopologySpec.hs
@@ -15,20 +15,20 @@ spec = do
     describe "addFreeVertex" $
         it "Is inversed by removeVertex" $ do
             let run = addFreeVertex >>= removeVertex
-            property (prop_stateIdentity run)
+            property (prop_runIdentity run)
     describe "vertexID" $ do
         it "provides a numeric ID that can be used to re-create the Vertex" $ do
             let run = runMaybeT $ do vertex  <- lift addFreeVertex
                                      vid     <- MaybeT (vertexID vertex)
                                      vertex' <- MaybeT (vertexFromID vid)
                                      pure (vertex == vertex')
-            property (prop_stateExpect run (Just True))
+            property (prop_runExpect run (Just True))
         it "returns Nothing if the numeric ID is invalid" $ do
             let run = do topo    <- get
                          vertex  <- addFreeVertex
                          put topo -- vid should be invalid now
                          vertexID vertex
-            property (prop_stateExpect run Nothing)
+            property (prop_runExpect run Nothing)
     describe "addEdge" $ do
         let prep = do v1 <- addFreeVertex
                       v2 <- addFreeVertex
@@ -36,38 +36,38 @@ spec = do
             run  = uncurry addEdge
         it "is inversed by removeedge" $ do
             let run' = run >=> traverse_ removeEdge
-            property (prop_prepStateIdentity prep run')
+            property (prop_prepRunIdentity prep run')
         context "returns Nothing if" $ do
             let prep' = prep >>= \vals -> removeVertex (snd vals) >> pure vals
             it "the first vertex doesn't exist" $
-                property (prop_prepStateExpect prep' run Nothing)
+                property (prop_prepRunExpect prep' run Nothing)
             it "the second vertex doesn't exist" $
-                property (prop_prepStateExpect prep' (run . swap) Nothing)
+                property (prop_prepRunExpect prep' (run . swap) Nothing)
         it "creates an Out adjacency from v1 → Edge" $ do
             let run' vs@(v1,_)= runMaybeT $ do
                     edge <- MaybeT (run vs)
                     lift (vertexEdges v1) >>= pure . ([Out edge] ==)
-            property (prop_prepStateExpect prep run' (Just True))
+            property (prop_prepRunExpect prep run' (Just True))
         it "creates an In adjacency for Edge ← v1" $ do
             let run' vs@(v1,_) = runMaybeT $ do
                     edge <- MaybeT (run vs)
                     lift (edgeVertices edge) >>= pure . (elem (In v1))
-            property (prop_prepStateExpect prep run' (Just True))
+            property (prop_prepRunExpect prep run' (Just True))
         it "creates an Out adjacency for Edge → v2" $ do
             let run' vs@(_,v2) = runMaybeT $ do
                     edge <- MaybeT (run vs)
                     lift (edgeVertices edge) >>= pure . (elem (Out v2))
-            property (prop_prepStateExpect prep run' (Just True))
+            property (prop_prepRunExpect prep run' (Just True))
         it "creates an In adjacency from v2 ← Edge" $ do
             let run' vs@(_,v2)= runMaybeT $ do
                     edge <- MaybeT (run vs)
                     lift (vertexEdges v2) >>= pure . ([In edge] ==)
-            property (prop_prepStateExpect prep run' (Just True))
+            property (prop_prepRunExpect prep run' (Just True))
         xit "returns the same Edge if called twice with v1→v2" $ do
             let run' args = do edge  <- run args
                                edge' <- run args
                                pure (edge == edge')
-            property (prop_prepStateExpect prep run' True)
+            property (prop_prepRunExpect prep run' True)
 
 -- ===========================================================================
 --                            Properties
@@ -76,8 +76,8 @@ spec = do
 type TopoMod a b= a -> TopoState b
 
 -- The given state should not modify anything when executed
-prop_stateIdentity :: TopoState a -> TestTopology -> Bool
-prop_stateIdentity run = prop_prepStateIdentity prep run'
+prop_runIdentity :: TopoState a -> TestTopology -> Bool
+prop_runIdentity run = prop_prepRunIdentity prep run'
     where prep  = pure ()
           run' a = const prep a >> run
 
@@ -85,22 +85,22 @@ prop_stateIdentity run = prop_prepStateIdentity prep run'
 -- on to the TopoMod.
 --
 -- The TopoMod should not modify the state
-prop_prepStateIdentity :: TopoState a -> TopoMod a b -> TestTopology -> Bool
-prop_prepStateIdentity prep run testTopology = initialState == finalState
+prop_prepRunIdentity :: TopoState a -> TopoMod a b -> TestTopology -> Bool
+prop_prepRunIdentity prep run testTopology = initialState == finalState
     where topology = unTestTopology testTopology
           initialState = execState prep topology
           finalState = execState (prep >>= run) topology
 
 -- The given TopoMod should produce the given output.
-prop_stateExpect :: Eq a => TopoState a -> a -> TestTopology -> Bool
-prop_stateExpect run val = prop_prepStateExpect prep run' val
+prop_runExpect :: Eq a => TopoState a -> a -> TestTopology -> Bool
+prop_runExpect run val = prop_prepRunExpect prep run' val
     where prep = pure ()
           run' _ = run
 
 -- The given TopoMod should produce the given output. The TopoMod is "prepped"
 -- by running the prep state, and passing along the result
-prop_prepStateExpect :: Eq b => TopoState a -> TopoMod a b -> b -> TestTopology -> Bool
-prop_prepStateExpect prep run val testTopology = val == producedVal
+prop_prepRunExpect :: Eq b => TopoState a -> TopoMod a b -> b -> TestTopology -> Bool
+prop_prepRunExpect prep run val testTopology = val == producedVal
     where producedVal = evalState (prep >>= run) (unTestTopology testTopology)
 
 
