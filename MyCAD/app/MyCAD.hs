a50e858e6d7d76c83c0858e5dc06323eab533be9
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-07-31 16:30:31 -0400

    Added renderEntity to MyCAD main. it does nothing right now.

--- MyCAD/app/MyCAD.hs
+++ MyCAD/app/MyCAD.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 -- Base
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM.TMVar (TMVar)
 
 -- Internal
@@ -14,7 +14,8 @@ main :: IO ()
 main = do entity <- TUI.initialize :: IO (TMVar (Entity Float))
           queue  <- GUI.initialize
 
-          forkIO (runTUI queue entity)
+          forkIO (runTUI entity)
+          forkIO (renderEntity entity queue)
           GUI.launch queue
 
           -- TODO: Use GUI.RenderQueue.queueObject with queue well as
@@ -27,7 +28,14 @@ main = do entity <- TUI.initialize :: IO (TMVar (Entity Float))
           -- TODO: Catch quit signal from TUI and shut down GUI.
 
 runTUI :: (Show a, Fractional a, Eq a)
-       => RenderQueue
-       -> TMVar (Entity a)
+       => TMVar (Entity a)
        -> IO ()
-runTUI _ = TUI.launch
+runTUI = TUI.launch
+
+renderEntity :: (Show a, Fractional a, Eq a)
+            => TMVar (Entity a)
+            -> RenderQueue
+            -> IO ()
+renderEntity entityVar queue = do
+    threadDelay 100
+    renderEntity entityVar queue
