df51ada464469764dc1e908eba1ad2cee0717132
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-08-05 16:50:06 -0400

    Squash up, fixed EntitySpec forrill

--- Entity/test/EntitySpec.hs
+++ Entity/test/EntitySpec.hs
@@ -1,5 +1,6 @@
 module EntitySpec (spec) where
 
+import Data.Either (isLeft)
 import Entity
 import Test.Hspec (Spec, describe, it)
 import Test.QuickCheck ((==>), Arbitrary, arbitrary, listOf, property, Property)
@@ -7,8 +8,7 @@ import qualified Geometry as Geo
 import Linear.V3 (V3(V3))
 import Control.Monad.State (runState, execState, evalState, gets)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Except (ExceptT, runExceptT, throwError)
-import Data.Maybe (isNothing)
+import Control.Monad.Except (runExceptT, throwError, liftEither)
 
 spec :: Spec
 spec = do
@@ -32,16 +32,19 @@ prop_addEdgeGetLine :: TestPoint Float -> TestPoint Float -> TestEntity Float ->
 prop_addEdgeGetLine (TestPoint p1) (TestPoint p2) (TestEntity entity) =
     p1 /= p2 ==> evalState eState entity == Right line
     where eState = runExceptT $ do
+              -- First, get add the two vertices given the points from QuickCheck
               v1 <- lift (addVertex p1)
               v2 <- lift (addVertex p2)
-              edge <- lift (addEdge v1 v2)
-                  :: ExceptT String (EntityState p) (Geo.Line p)
-              lift $ (gets (`getCurve` edge)) >>= maybe (Left "") Right
+              -- Try to add the Edge. This should always succeed.
+              edge <- lift (addEdge v1 v2) >>= liftEither
+              -- Try to retrieve the Line
+              lift (gets (flip getCurve edge))
+                  >>= maybe (throwError "failure") pure
           line = Geo.makeLine p1 p2
 
 prop_addEdgeFail :: TestPoint Float -> TestEntity Float -> Bool
 prop_addEdgeFail (TestPoint point) (TestEntity entity) =
-    isNothing (evalState eState entity)
+    isLeft (evalState eState entity)
     where eState = addVertex point >>= \v -> addEdge v v
 
 -- ===========================================================================
