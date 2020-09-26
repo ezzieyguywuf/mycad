2d9979de1b745220aa1a6ae16c96a10f980ad702
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-09-26 16:21:34 -0400

    Fixed typo, squash up.

--- Topology/test/TopologySpec.hs
+++ Topology/test/TopologySpec.hs
@@ -106,7 +106,7 @@ spec = do
         it "is inversed by removeFace" $ do
             let run' = run >=> traverse_ removeFace
             property (prop_prepRunIdentity prep run')
-        it "returns Right of the Edges are contigous and form a loop" $ do
+        it "returns Right if the Edges are contigous and form a loop" $ do
             let post = pure . isRight . snd
             property (prop_prepRunPostExpect prep run post)
         
